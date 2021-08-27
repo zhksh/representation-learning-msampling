fbb1fa88ccb57e7c4159ef4a01a0d705bbc5b141
Pascal Guldener <pascal@guldener.io>
2021-08-27 23:22:22 +0200

    redirect all output to respective files

--- run_exp.sh
+++ run_exp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./train_bert.py data/train.tsv --sample up --name sampleup_cls --num_epochs 10 --desc "bert with upsampled no cls" >> bert_log
-./train_deberta.py data/train.tsv --sample up --name sampleup_cls --num_epochs 10 --desc "bert with upsampled no cls" >> deberta_log
-./train_distilbert.py data/train.tsv --sample up --name sampleup_cls --num_epochs 10 --desc "bert with upsampled no cls" >> distilbert_log
+./train_bert.py data/train.tsv --sample up --name sampleup_cls --num_epochs 10 --desc "bert with upsampled no cls" &> bert_log
+./train_deberta.py data/train.tsv --sample up --name sampleup_cls --num_epochs 10 --desc "bert with upsampled no cls" &> deberta_log
+./train_distilbert.py data/train.tsv --sample up --name sampleup_cls --num_epochs 10 --desc "bert with upsampled no cls" &> distilbert_log
