33228750e9d20b7f305a9a3aaf909c2084b5fbf1
Pascal Guldener <pascal@guldener.io>
2021-08-29 17:32:28 +0200

    i

--- prep_amazon_data.py
+++ prep_amazon_data.py
@@ -8,22 +8,27 @@ conf = utils.read_conf()
 
 
 if __name__ == "__main__":
-    json_data = []
-    with open(conf.train_file, 'r') as f:
-        for line in f.readlines():
-            line = line.replace('\n', '')
-            item = json.loads(line)
-            try:
-                if 'reviewText' not in item : continue
-                item['overall'] -= 1
-                json_data.append({'Sentiment' :item['overall'], 'Phrase': item['reviewText']})
-            except Exception as e:
-                print(line)
-                print(e)
-    data = pd.DataFrame(json_data)
-    del json_data
-    plt = utils.show_dist_plot(data.Sentiment.values, "Class distribution before " +conf.sample+"sampling")
-    plt.savefig("{}{}.png".format(conf.train_file, "_classdist_before"))
+    data = None
+    if conf.reload or not utils.exists("data/test_reference_full.tsv"):
+        json_data = []
+        with open(conf.train_file, 'r') as f:
+            for line in f.readlines():
+                line = line.replace('\n', '')
+                item = json.loads(line)
+                try:
+                    if 'reviewText' not in item : continue
+                    item['overall'] -= 1
+                    json_data.append({'Sentiment' :item['overall'], 'Phrase': item['reviewText']})
+                except Exception as e:
+                    print(line)
+                    print(e)
+        data = pd.DataFrame(json_data)
+        del json_data
+        plt = utils.show_dist_plot(data.Sentiment.values, "Class distribution before " +conf.sample+"sampling")
+        plt.savefig("{}{}.png".format(conf.train_file, "_classdist_before"))
+    else:
+        data = pd.read_csv("data/test_reference_full.tsv", delimiter='\t', usecols = ['Phrase', 'Sentiment'])
+
 
     sampled = utils.sample_data(data, "Sentiment", conf.sample)
 
