8c100a913ba6a685b1afe2b2349b3b8aa7e73b91
Pascal Guldener <pascal@guldener.io>
2021-08-29 18:24:57 +0200

    i

--- BertSentimentWithHead.py
+++ BertSentimentWithHead.py
@@ -22,21 +22,23 @@ class BertSentimentWithHead(ExperimentBase):
         return self.base_model(*args, **kwargs)
 
 
-    def evaluate(self, data_loader):
+    def evaluate(self, data_loader, device=None):
         self.eval()
-        device = torch.device('cpu')
+        if device is None:
+            device = self.device
+        # device = torch.device('cpu')
         self.to(device)
         accuracy_acc = loss_acc = 0
         losses = []
         with tqdm(data_loader, unit="batch") as batch_generator:
             batch_generator.set_description("Evaluation")
             for c, batch in enumerate(batch_generator, 1):
-                X = batch[0]
-                X_mask = batch[1]
-                Y = batch[2]
+                X = batch[0].to(device)
+                X_mask = batch[1].to(device)
+                Y = batch[2].to(device)
 
-                with torch.no_grad():
-                    output = self(X, attention_mask=X_mask, labels=Y)
+            with torch.no_grad():
+                output = self(X, attention_mask=X_mask, labels=Y)
                 loss_acc += output.loss.item()
                 losses_batch_avg = loss_acc / c
                 losses.append(losses_batch_avg)
