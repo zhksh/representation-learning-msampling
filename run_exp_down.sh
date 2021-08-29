96eafbf6b598494bd41ae50e8d8836f638f9ca58
Pascal Guldener <pascal@guldener.io>
2021-08-30 01:22:12 +0200

    i

--- run_exp_down.sh
+++ run_exp_down.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./train_bert.py data/train.tsv --sample down --max_length 150--name sampledown --num_epochs 10 --desc "bert with downsampled no cls" &> bert_log__down
-./train_deberta.py data/train.tsv --sample down --max_length 150--name sampledown --num_epochs 10 --desc "bert with downsampled no cls" &> deberta_log_down
-./train_distilbert.py data/train.tsv --sample down --max_length 150--name sampledown --num_epochs 10 --desc "bert with downsampled no cls" &> distilbert_log_down
+./train_bert.py data/train.tsv --sample down --max_length 165 --name sampledown --num_epochs 10 --desc "bert with downsampled no cls" &> bert_log__down
+./train_deberta.py data/train.tsv --sample down --max_length 165 --name sampledown --num_epochs 10 --desc "bert with downsampled no cls" &> deberta_log_down
+./train_distilbert.py data/train.tsv --sample down --max_length 165 --name sampledown --num_epochs 10 --desc "bert with downsampled no cls" &> distilbert_log_down
 
 
 #./train_bert.py data/train.tsv --sample up --name sampleup_cls --num_epochs 10 --desc "bert with upsampled no cls" &> bert_log
