6d4c36b06244ce45ee99c998be4ea35b60e241f5
Pascal Guldener <pascal@guldener.io>
2021-08-29 17:47:48 +0200

    ii

--- eval.py
+++ eval.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python
-import argparse
 
-import pandas as pd
-import transformers
+import torch
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler
-from transformers import BertTokenizer, logging
+from transformers import logging
 
-import utils
 from utils import *
 
 logging.set_verbosity_error()
@@ -21,16 +18,16 @@ print(conf)
 
 
 if __name__ == '__main__':
-    # tokenizer = BertTokenizer.from_pretrained(conf.model_name)
-    # data_eval = pd.read_csv(conf.eval_file, delimiter='\t',usecols = ['Phrase','Sentiment'])
-    # ids, masks = preprocess_sentences(data_eval.Phrase.values, tokenizer)
-    # X = torch.cat(ids, dim=0)
-    # X_mask = torch.cat(masks, dim=0)
-    # Y = torch.tensor(data_eval.Sentiment.values)
-    # test_dataset = TensorDataset(X, X_mask, Y)
-    # test_loader = DataLoader(test_dataset, sampler = RandomSampler(test_dataset), batch_size = 1)
-
     model = torch.load(conf.path + "/model.torch")
+
+    data_eval = pd.read_csv(conf.eval_file, delimiter='\t',usecols = ['Phrase','Sentiment'])
+    ids, masks = model.preprocess_sentences(data_eval.Phrase.values, model.tokenizer)
+    X = torch.cat(ids, dim=0)
+    X_mask = torch.cat(masks, dim=0)
+    Y = torch.tensor(data_eval.Sentiment.values)
+    test_dataset = TensorDataset(X, X_mask, Y)
+    test_loader = DataLoader(test_dataset, sampler = RandomSampler(test_dataset), batch_size = 1)
+
     accuracy = model.evaluate(model.test_loader, torch.nn.CrossEntropyLoss())
 
 
