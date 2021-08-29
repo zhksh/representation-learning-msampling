3177e7b91d5db35c40db5d450a64237e706411c0
Pascal Guldener <pascal@guldener.io>
2021-08-29 17:29:37 +0200

    reduce set ref stratigied

--- prep_amazon_data.py
+++ prep_amazon_data.py
@@ -29,4 +29,11 @@ if __name__ == "__main__":
 
     plt = utils.show_dist_plot(sampled.Sentiment.values, "Class distribution after " +conf.sample+"sampling")
     plt.savefig("{}{}.png".format(conf.train_file, "_classdist_after"))
-    sampled.to_csv('data/test_reference.tsv', sep = '\t')
+    sampled.to_csv('data/test_reference_full.tsv', sep = '\t')
+
+    split = utils.data_split(sampled.Phrase.values, sampled.Seniment.values, "data/reference")
+    # this will give as a stratified subset, the original set is really large
+    reduced_data = pd.DataFrame({'Sentiment': split['Y_test'], 'Phrase': split['X_test']})
+    plt = utils.show_dist_plot(sampled.Sentiment.values, "Class distribution of subset after " +conf.sample+"sampling")
+    plt.savefig("{}{}.png".format(conf.train_file, "_subset_classdist_after"))
+    reduced_data.to_csv('data/test_reference.tsv', sep = '\t')
