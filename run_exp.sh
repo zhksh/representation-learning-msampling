5171887939cc4db202fee459f5c1c1b6b3ef826b
Pascal Guldener <pascal@guldener.io>
2021-08-30 21:29:46 +0200

    i

--- run_exp.sh
+++ run_exp.sh
@@ -6,9 +6,9 @@ mode="cls"
 epochs=7
 data="data/train.tsv"
 cross_eval="data/test_reference.tsv"
-./train_bert.py $data --sample $middle --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> bert_log_$name
-./train_deberta.py $data --sample $middle --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "deberta with no cls annotated data" &> deberta_log_$name
-./train_distilbert.py $data --sample $middle --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> distilbert_log_$name
+./train_bert.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> bert_log_$name
+./train_deberta.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "deberta with no cls annotated data" &> deberta_log_$name
+./train_distilbert.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> distilbert_log_$name
 
 
 sample="middle"
@@ -17,9 +17,9 @@ mode="cls"
 epochs=7
 data="data/train_cls.tsv"
 cross_eval="data/test_referencecls.tsv"
-./train_bert.py $data --sample $middle --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> bert_log_$name
-./train_deberta.py $data --sample $middle --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "deberta with no cls annotated data" &> deberta_log_$name
-./train_distilbert.py $data --sample $middle --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> distilbert_log_$name
+./train_bert.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> bert_log_$name
+./train_deberta.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "deberta with no cls annotated data" &> deberta_log_$name
+./train_distilbert.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> distilbert_log_$name
 
 sample="middle"
 name="avg_middle_sample"
@@ -27,9 +27,9 @@ mode="avg"
 epochs=7
 data="data/train.tsv"
 cross_eval="data/test_reference.tsv"
-./train_bert.py $data --sample $middle --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> bert_log_$name
-./train_deberta.py $data --sample $middle --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "deberta with no cls annotated data" &> deberta_log_$name
-./train_distilbert.py $data --sample $middle --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> distilbert_log_$name
+./train_bert.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> bert_log_$name
+./train_deberta.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "deberta with no cls annotated data" &> deberta_log_$name
+./train_distilbert.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> distilbert_log_$name
 
 sample="down"
 name="cls_cls_data_down"
@@ -37,6 +37,6 @@ mode="cls"
 epochs=7
 data="data/train_cls.tsv"
 cross_eval="data/test_referencecls.tsv"
-./train_bert.py $data --sample $middle --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> bert_log_$name
-./train_deberta.py $data --sample $middle --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "deberta with no cls annotated data" &> deberta_log_$name
+./train_bert.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> bert_log_$name
+./train_deberta.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "deberta with no cls annotated data" &> deberta_log_$name
 ./train_distilbert.py $data --sample $middle --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> distilbert_log_$name
