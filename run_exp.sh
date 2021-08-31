25a20ddd48bd3c7421d919ff16fe61d7d6309f78
Pascal Guldener <pascal@guldener.io>
2021-08-31 09:36:45 +0200

    i

--- run_exp.sh
+++ run_exp.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+sample="down"
+name="cls_cls_data_down"
+mode="cls"
+epochs=4
+data="data/train_cls.tsv"
+cross_eval="data/test_referencecls.tsv"
+./train_distilbert.py $data --sample $middle --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> distilbert_log_$name
+./train_bert.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> bert_log_$name
+./train_deberta.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "deberta with no cls annotated data" &> deberta_log_$name
 
 sample="middle"
 name="cls_nocls_data"
@@ -31,12 +40,3 @@ cross_eval="data/test_reference.tsv"
 ./train_deberta.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "deberta with no cls annotated data" &> deberta_log_$name
 ./train_distilbert.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> distilbert_log_$name
 
-sample="down"
-name="cls_cls_data_down"
-mode="cls"
-epochs=4
-data="data/train_cls.tsv"
-cross_eval="data/test_referencecls.tsv"
-./train_bert.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> bert_log_$name
-./train_deberta.py $data --sample $sample --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "deberta with no cls annotated data" &> deberta_log_$name
-./train_distilbert.py $data --sample $middle --name $name --class_mode $mode --cross_eval_file $cross_eval --num_epochs $epochs --desc "bert with no cls annotated data" &> distilbert_log_$name
